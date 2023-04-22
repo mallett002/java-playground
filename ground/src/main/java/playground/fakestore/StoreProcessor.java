3f62a39337bfde59506990ba4064cac14b0556e1
William Mallett <mallett002@gmail.com>
2023-04-22 14:58:54 -0500

    finish store with highest inventory

--- ground/src/main/java/playground/fakestore/StoreProcessor.java
+++ ground/src/main/java/playground/fakestore/StoreProcessor.java
@@ -2,6 +2,7 @@ package playground.fakestore;
 
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -17,65 +18,69 @@ public class StoreProcessor {
 
     public Store storeWithHighestInventory() {
         List<Item> items = storeFactory.getItems();
-        TreeMap<Integer, String> inventoryCountMap = new TreeMap<>();
 
-        storeFactory.getStores()
-                .forEach((store) -> {
-                    Integer sumA = 0;
-                    Integer sumB = 0;
+        return storeFactory.getStores().stream()
+                .sorted((store1, store2) -> {
+                    Map<UUID, Integer> storeCount = items.stream()
+                            .map(Item::getLocations)
+                            .flatMap(List::stream)
+                            .filter(storeId -> storeId.equals(store1.getId()) || storeId.equals(store2.getId()))
+                            .reduce(
+                                    new HashMap<UUID, Integer>(),
+                                    (partial, storeId) -> {
+                                        if (partial.containsKey(storeId)) {
+                                            Integer countOne = partial.get(storeId);
+                                            partial.put(storeId, countOne + 1);
+                                        } else {
+                                            partial.put(storeId, 1);
+                                        }
 
-                    for (Item item : items) {
-                        for (UUID storeId : item.getLocations()) {
-                            if (storeId.equals(store.getId())) {
-                                sumA++;
-                            }
-                            if (storeId.equals(store.getId())) {
-                                sumB++;
-                            }
-                        }
-                    }
-                    inventoryCountMap.put(sumA, store.getStoreName());
-                    inventoryCountMap.put(sumB, store.getStoreName());
-                });
+                                        return partial;
+                                    },
+                                    (mapOne, mapTwo) -> {
+                                        mapOne.putAll(mapTwo);
+                                        return mapOne;
+                                    }
+                            );
 
-
-//        List<UUID> it =
-        TreeMap<String, Integer> it = storeFactory.getItems().stream()
-                    .map(Item::getLocations)
-                    .flatMap(List::stream)
-                    .map((id) -> {
-                        Store foundStore = storeFactory.getStores().stream()
-                                .filter(store -> store.getId().equals(id)).findAny().orElse(null);
-
-                        return foundStore != null ? foundStore.getStoreName() : null;
-                    })
-                    .filter(Objects::nonNull)
-                    .reduce(
-                            new TreeMap<>(),
-                            (partial, storeName) -> {
-                                if (partial.containsKey(storeName)) {
-                                    Integer curr = partial.get(storeName);
-                                    partial.put(storeName, curr + 1);
-                                } else {
-                                    partial.put(storeName, 1);
-                                }
-
-                                return partial;
-                            },
-                            (first, second) -> {
-                                first.putAll(second);
-
-                                return first;
-                            }
-                    );
-
-        int max = it.values().stream().max(Integer::compare).get();
-        String maxKey = it.keySet().stream().min((a, b) -> it.get(b).compareTo(it.get(a)))
+                    return storeCount.get(store2.getId()).compareTo(storeCount.get(store1.getId()));
+                })
+                .findFirst()
                 .orElse(null);
 
-//                        .collect(Collectors.toMap(Animal::getId, Function.identity()));
 
-
-        return null;
+        // Alternative way:
+//        TreeMap<String, Integer> inventoryCountsByStoreId = storeFactory.getItems().stream()
+//                    .map(Item::getLocations)
+//                    .flatMap(List::stream)
+//                    .map((id) -> {
+//                        Store foundStore = storeFactory.getStores().stream()
+//                                .filter(store -> store.getId().equals(id)).findAny().orElse(null);
+//
+//                        return foundStore != null ? foundStore.getStoreName() : null;
+//                    })
+//                    .filter(Objects::nonNull)
+//                    .reduce(
+//                            new TreeMap<>(),
+//                            (partial, storeName) -> {
+//                                if (partial.containsKey(storeName)) {
+//                                    Integer curr = partial.get(storeName);
+//                                    partial.put(storeName, curr + 1);
+//                                } else {
+//                                    partial.put(storeName, 1);
+//                                }
+//
+//                                return partial;
+//                            },
+//                            (first, second) -> {
+//                                first.putAll(second);
+//
+//                                return first;
+//                            }
+//                    );
+//
+//        int largestInventoryCount = inventoryCountsByStoreId.values().stream().max(Integer::compare).get();
+//        String expectedStoreWithLargestInventory = inventoryCountsByStoreId.keySet().stream().min((a, b) -> inventoryCountsByStoreId.get(b).compareTo(inventoryCountsByStoreId.get(a)))
+//                .orElse(null);
     }
 }
